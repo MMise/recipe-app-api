9ac2126c4d827036a41afd17ed968e6a741e1bde
Mikko Jokinen <mikko.jokinen@boogiesoftware.com>
2022-03-21 16:35:12 +0200

    Create tag feature implemented

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -62,3 +62,21 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tags_successful(self):
+        """Test that tag creation works."""
+        payload = {
+            'name': 'TestTag'
+        }
+
+        self.client.post(TAG_URL, payload)
+        self.assertTrue(
+            Tag.objects.filter(user=self.user, name=payload['name']).exists()
+        )
+
+    def test_create_tag_invalid(self):
+        """Test creating a tag with invalid name."""
+        payload = {'name': ''}
+
+        res = self.client.post(TAG_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
