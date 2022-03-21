58bf8c27247bdea7d3f5cbe66baef3a2ef4b1d77
Mikko Jokinen <mikko.jokinen@boogiesoftware.com>
2022-03-21 17:12:09 +0200

    List ingredients feature added

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -3,9 +3,9 @@ from rest_framework.routers import DefaultRouter
 
 from . import views
 
-
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
 
 app_name = 'recipe'
 
